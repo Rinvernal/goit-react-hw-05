31cf4d816702d306b5f51a45049bfd85d6fb5a31
Rinvernal <irina.elkina2006@gmail.com>
2024-11-25 11:22:05 +0200

    example

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,14 @@
-import Profile from "./Profile/Profile";
-import userData from "../userData.json";
-import friends from "../friends.json"
-import FriendList from "./FriendList/FriendList"
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
-import transactions from "../transactions.json"
+import { Route, Routes } from "react-router-dom"
 
 const App = () => {
   return (
     <div>
-      <Profile 
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        img={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList 
-        friends={friends}/>
-      <TransactionHistory
-        items={transactions}/>
+      <Routes>
+        <Route path="/" element={<h2>Home page</h2>}/>
+      </Routes>
+
     </div>
-      
   )
 }
 
-export default App;
+export default App
